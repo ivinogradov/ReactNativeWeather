e8b631823b31005b2437a2559c5452cd4fad7575
Ilya Vinogradov <ilya.vngrdv@gmail.com>
2025-03-24 20:06:08 -0500

    configured eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,33 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import reactNativeCommunity from '@react-native-community/eslint-config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: { ...globals.browser, es2021: true },
+    },
+    plugins: { react: pluginReact },
+    rules: {
+      semi: ['error', 'never'],
+      'comma-dangle': ['error', 'never'],
+      'react/jsx-filename-extension': [
+        'error',
+        { extensions: ['.js', '.jsx'] },
+      ],
+      'no-use-before-define': [
+        'error',
+        { functions: true, classes: true, variables: false },
+      ],
+    },
+  },
+
+  js.configs.recommended,
   pluginReact.configs.flat.recommended,
+  reactNativeCommunity,
 ]);
